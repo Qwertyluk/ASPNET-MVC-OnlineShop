06745a55d47eb919195c653537a92da0fbf464a2
Qwertyluk <64921645+Qwertyluk@users.noreply.github.com>
2020-08-12 20:16:25 +0200

    Add attributes for category model
    Added some attributes for category model.

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,10 @@ namespace OnlineShop.Models
 {
     public class Category
     {
+        [ScaffoldColumn(false)]
         public int CategoryId { get; set; }
+        [Required(ErrorMessage = "Category name is required.")]
+        [StringLength(50)]
         public string Name { get; set; }
         public string Description { get; set; }
     }

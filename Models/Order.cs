8f85d29094b7de1f8ae894be1745abef1321b6b2
Qwertyluk <64921645+Qwertyluk@users.noreply.github.com>
2020-08-12 19:08:47 +0200

    Add basic models - Product, Order and Category.
    Added basic model such as product, order and category for future work.

---
+++ Models/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string PurchaserName { get; set; }
+        public string PurchaserSurname { get; set; }
+        public string PurchaserStreet { get; set; }
+        public string PurchaserCity { get; set; }
+        public string PurchaserPhoneNumber { get; set; }
+        public string PurchaserEmail { get; set; }
+        public string Comment { get; set; }
+        public DateTime OrderDate { get; set; }
+        public OrderState StateOfOrder { get; set; }
+        public decimal Value { get; set; }
+    }
+
+    public enum OrderState
+    {
+        NotStarted,
+        InProgress,
+        Ended
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsSold { get; set; }
+        public bool IsOnSale { get; set; }
+    }
+}

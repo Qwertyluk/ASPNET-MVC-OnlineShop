d8f945fafb23126f4e48c095774721e0a5b1d21a
Qwertyluk <64921645+Qwertyluk@users.noreply.github.com>
2020-09-15 20:47:27 +0200

    Allow user to change profile and password
    Added manage controller with profile and password change actions. Added
    also account management view and its model.

---
+++ OnlineShop/Controllers/ManageController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using OnlineShop.App_Start;
+using OnlineShop.Models;
+using OnlineShop.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Controllers
+{
+    public class ManageController : Controller
+    {
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            set
+            {
+                _userManager = value;
+            }
+        }
+
+        // GET: Manage
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+
+            return View(new ManageProfileViewModel()
+            {
+                ManageProfile = new ManageProfile()
+                {
+                    Name = user.User.Name,
+                    Surname = user.User.Surname,
+                    Adress = user.User.Adress,
+                    PostCode = user.User.PostCode,
+                    Email = user.Email,
+                    Phone = user.PhoneNumber,
+                    ErrorOccured = null
+                },
+            });
+        }
+
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> UpdateProfile(ManageProfileViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+                user.User.Name = model.ManageProfile.Name;
+                user.User.Surname = model.ManageProfile.Surname;
+                user.User.Adress = model.ManageProfile.Adress;
+                user.User.PostCode = model.ManageProfile.PostCode;
+                user.Email = model.ManageProfile.Email;
+                user.PhoneNumber = model.ManageProfile.Phone;
+                var result = await UserManager.UpdateAsync(user);
+
+                model.ManageProfile.ErrorOccured = !result.Succeeded;
+
+                if (model.ManageProfile.ErrorOccured.Value)
+                    AddErrors(result);
+            }
+
+            return View("Index", model);
+        }
+
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> UpdatePassword(ManagePassword ManagePassword)
+        {
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            ManageProfileViewModel modelReturn = new ManageProfileViewModel()
+            {
+                ManageProfile = new ManageProfile()
+                {
+                    Name = user.User.Name,
+                    Surname = user.User.Surname,
+                    Adress = user.User.Adress,
+                    PostCode = user.User.PostCode,
+                    Email = user.Email,
+                    Phone = user.PhoneNumber,
+                    ErrorOccured = null
+                },
+                ManagePassword = ManagePassword
+            };
+
+            if (ModelState.IsValid)
+            {
+                var result = await UserManager.ChangePasswordAsync(User.Identity.GetUserId(), ManagePassword.ActualPassword, ManagePassword.NewPassword);
+                modelReturn.ManagePassword.ErrorOccured = !result.Succeeded;
+                if (modelReturn.ManagePassword.ErrorOccured.Value)
+                    AddErrors(result);
+            }
+
+            return View("Index", modelReturn);
+        }
+
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("Validation", error);
+            }
+        }
+    }
+}
---
+++ OnlineShop/Models/ViewModels/ManageProfileViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Models.ViewModels
+{
+    public class ManageProfileViewModel
+    {
+        public ManageProfile ManageProfile { get; set; }
+        public ManagePassword ManagePassword { get; set; }
+    }
+
+    public class ManageProfile
+    {
+        [Display(Name = "Imie")]
+        [StringLength(30, ErrorMessage = "Imie nie moze miec wiecej niz 30 znakow")]
+        public string Name { get; set; }
+
+        [Display(Name = "Nazwisko")]
+        [StringLength(30, ErrorMessage = "Nazwisko nie moze miec wiecej niz 30 znakow")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Adres zamieszkania")]
+        [StringLength(100, ErrorMessage = "Adres nie moze miec wiecej niz 100 znakow")]
+        public string Adress { get; set; }
+
+        [Display(Name = "Kod pocztowy")]
+        [RegularExpression("[0-9]{2}-[0-9]{3}", ErrorMessage = "Kod jest w formacie XX-XXX")]
+        public string PostCode { get; set; }
+
+        [Display(Name = "Adres E-mail")]
+        [EmailAddress]
+        [Required(ErrorMessage = "Pole jest wymagane")]
+        public string Email { get; set; }
+
+        [Display(Name = "Numer telefonu")]
+        public string Phone { get; set; }
+
+        [ScaffoldColumn(false)]
+        public bool? ErrorOccured { get; set; }
+    }
+
+    public class ManagePassword
+    {
+        [Required(ErrorMessage = "Aktualne hasło jest wymagane")]
+        [Display(Name = "Aktualne hasło")]
+        [StringLength(30, ErrorMessage = "Hasło nie może mieć więcej niż 30 znaków")]
+        [DataType(DataType.Password)]
+        public string ActualPassword { get; set; }
+
+        [Required(ErrorMessage = "Nowe hasło jest wymagane")]
+        [Display(Name = "Nowe hasło")]
+        [StringLength(30, ErrorMessage = "Hasło nie może mieć więcej niż 30 znaków")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Compare("NewPassword", ErrorMessage = "Nieprawidlowe haslo")]
+        [Display(Name = "Potwierdź nowe hasło")]
+        [DataType(DataType.Password)]
+        public string ConfirmNewPassword { get; set; }
+
+        [ScaffoldColumn(false)]
+        public bool? ErrorOccured { get; set; }
+    }
+}
